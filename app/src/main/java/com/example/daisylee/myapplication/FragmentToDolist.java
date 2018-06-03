516b5acf7f8a7c7cb52eb50315a6b60ef50ad448
DaisyLee0823 <lspss93055@gmail.com>
2018-06-03 10:09:51 +0800

    to do list

--- app/src/main/java/com/example/daisylee/myapplication/FragmentToDolist.java
+++ app/src/main/java/com/example/daisylee/myapplication/FragmentToDolist.java
@@ -1,8 +1,10 @@
 package com.example.daisylee.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 public class FragmentToDolist extends Fragment {
 
     View view;
+
     public FragmentToDolist() {
     }
 
@@ -18,6 +21,16 @@ public class FragmentToDolist extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_tab_todolist,container,false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.addtodolistbutton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),AddToDoListActivity.class));
+
+            }
+        });
+
 
         return view;
     }
